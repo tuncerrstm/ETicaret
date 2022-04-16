0880b77846d6c67e89397bc992ce17f9a092f856
tuncerrstm <tuncerrstm@gmail.com>
2022-04-16 23:23:13 +0300

    UPDATE :100:

---
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll(bool tracking = true);     // Sorgu üzerinde çalıştığım için IQueruable kullanıyorum.
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true);  // Burada ise bir şarka uygun dataları elde ediyoruz. (Where şartı mayetinde kullanıyoruz.)
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true);
+        Task<T> GetByIdAsync(string id, bool tracking = true);
+    }
+}
